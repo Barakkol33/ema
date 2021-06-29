3be2e8d36d4de4afdd31ad2e1ffd4bcc7214ab0a
barak <barakkol@gmail.com>
2021-06-29 10:42:02 +0300

    Created V8Setup class

--- EMALib/test.py
+++ EMALib/test.py
@@ -3,6 +3,14 @@ from stubs import  BoardStub
 from collections.abc import Mapping
 
 
+class V8Setup(Setup):
+    def toggle_power(self):
+        self.set_button.press(duration=3)
+
+    def show_phone_book(self):
+        self.set_button.press(duration=0.5)
+
+
 def get_setup(button_mapping: Mapping[str, int], is_stub=False) -> Setup:
     if is_stub:
         board = BoardStub("Harta")
@@ -14,10 +22,20 @@ def get_setup(button_mapping: Mapping[str, int], is_stub=False) -> Setup:
     return v8_setup
 
 
-def main():
-    button_mapping = {"power": 1, "volume_up": 2, "volume_down": 3}
-    v8_setup = get_setup(is_stub=True, button_mapping=button_mapping)
+def integration_basic_test():
+    v8_button_mapping = {"set_button": 3, "answer_button": 5}
+    v8_setup = V8Setup.construct_from_button_mapping(port=4, button_mapping=v8_button_mapping)
+    v8_setup.toggle_power()
+
+
+def code_test():
+    v8_button_mapping = {"set": 3, "answer": 5}
+    v8_setup = get_setup(is_stub=True, button_mapping=v8_button_mapping)
     v8_setup.power.press()
+
+
+def main():
+    integration_basic_test()
 
 
 if __name__ == "__main__":
