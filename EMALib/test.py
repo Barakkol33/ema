d9b6200c9ce13f7cd112bb3bea718704433ee040
barak <barakkol@gmail.com>
2021-06-29 11:24:03 +0300

    Added CLI

--- EMALib/test.py
+++ EMALib/test.py
@@ -8,18 +8,21 @@ class V8Setup(Setup):
         self.set_button.press(duration=3)
 
     def show_phone_book(self):
-        self.set_button.press(duration=0.5)
+        self.set_button.press()
+
+    def answer(self):
+        self.answer_button.press()
 
 
 def get_setup(button_mapping: Mapping[str, int], is_stub=False) -> Setup:
     if is_stub:
         board = BoardStub("Harta")
         buttons = {key: Button(board=board, name=key, button_id=value) for key, value in button_mapping.items()}
-        v8_setup = Setup(board, buttons=buttons)
+        generic_setup = Setup(board, buttons=buttons)
     else:
-        v8_setup = Setup.construct_from_button_mapping(port=4, button_mapping=button_mapping)
+        generic_setup = Setup.construct_from_button_mapping(port=4, button_mapping=button_mapping)
 
-    return v8_setup
+    return generic_setup
 
 
 def integration_basic_test():
@@ -29,13 +32,28 @@ def integration_basic_test():
 
 
 def code_test():
-    v8_button_mapping = {"set": 3, "answer": 5}
-    v8_setup = get_setup(is_stub=True, button_mapping=v8_button_mapping)
-    v8_setup.power.press()
+    v8_button_mapping = {"set_button": 3, "answer_button": 5}
+    generic_setup = get_setup(is_stub=True, button_mapping=v8_button_mapping)
+    generic_setup.set_button.press()
+
+
+def cli():
+    v8_button_mapping = {"set_button": 3, "answer_button": 5}
+    board = BoardStub("Harta")
+    buttons = {key: Button(board=board, name=key, button_id=value) for key, value in v8_button_mapping.items()}
+    v8_stub_setup = V8Setup(board, buttons=buttons)
+
+    while True:
+        command = input(">>>")
+
+        if command == "exit":
+            break
+
+        v8_stub_setup.__getattribute__(command)()
 
 
 def main():
-    integration_basic_test()
+    cli()
 
 
 if __name__ == "__main__":
